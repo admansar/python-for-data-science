de075635c7d24ea62065d0243653bc611aaaf1be
Adnane Mansar <admansar@c1r7p6.1337.ma>
2024-05-01 15:54:45 +0100

    first commit

---
+++ ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "morrocco!")
+ft_set = {"Hello", "tetouan!"}
+ft_dict = {"Hello" : "1337!"}
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
